c6deff767ad150830d7210d5c6f015d2107f9c2e
mbsaloka <mbsaloka@gmail.com>
2025-11-04 23:33:21 +0700

    feat: chint

--- image-url-to-raw.py
+++ image-url-to-raw.py
@@ -24,24 +24,11 @@ def convert_github_url(url: str) -> str:
 # ==== Contoh penggunaan dengan list URL ====
 
 urls_awal = [
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Gear%20Motors%20%E2%80%93%20VLT%C2%AE%20OneGearDrive.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/IC2-Micro.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VACON%C2%AE%20100%20FLOW.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VACON%C2%AE%20100%20INDUSTRIAL.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VACON%C2%AE%2020.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VACON%C2%AE%20NXP%20Air%20Cooled.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20AQUA%20Drive%20FC-202.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Automation%20Drive%20FC-302.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Brake%20Resistor%20MCE%20101.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Common%20Mode%20Filter%20MCC%20105.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20HVAC%20Drive%20FC-102.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Line%20Reactor%20MCC%20103.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Midi%20Drive%20FC-280.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Sine-Wave%20Filter%20MCC%20101.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Soft%20Starter%20MCD%20202.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20Soft%20Starter%20MCD%20600.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%20dUdt%20Filter%20MCC%20102.jpg",
-  "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/VLT%C2%AE%E2%80%AFSoft%E2%80%AFStart%E2%80%AFController%E2%80%AFMCD%E2%80%AF100.jpg",
+    "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Automation%20%26amp%3B%20Control%20Gear.jpg",
+    "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Fuse%20%26%20Circuit%20Breakers.jpg",
+    "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Industrial%20Part.jpg",
+    "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Relay.jpg",
+    "https://github.com/mbsaloka/custom-wp-post-importer/blob/master/image/Switches.jpg",
 ]
 
 urls_raw = []
